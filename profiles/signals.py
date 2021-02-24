3f99bed3dc0b1a9819b872c8caaa520a87b750b1
sbx12 <sbx018@gmail.com>
2021-02-24 03:29:24 -0500

    Added signal to creaet token and profile

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,13 +1,19 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile
 from allauth.account.signals import user_signed_up
+from allauth.account.models import EmailAddress
 
+# Models
+from django.contrib.auth import get_user_model
+from profiles.models import Profile
+from rest_framework.authtoken.models import Token
 
-@receiver(user_signed_up)
-def create_profile(request, user, **kwargs):
-    Profile.objects.create(
-        user=user
-    )
-    print("Signal: New Profile Created")
+@receiver(post_save, sender=get_user_model())
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
+        if not (Token.objects.filter(user=instance).exists()):
+            Token.objects.create(user=instance)
+        print("Signal: New Profile Created")
