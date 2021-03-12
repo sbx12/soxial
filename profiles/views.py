ca6ee9ec84111ed03166d64e647867b071157209
sbx12 <sbx018@gmail.com>
2021-03-12 18:18:15 -0500

    Added and setup profile page

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 
 
 class ProfileView(generic.TemplateView):
-    template_name = "profiles/profile.html"
+    template_name = "profiles/profile_page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
